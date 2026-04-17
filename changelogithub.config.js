c14cee715c3afe7c770f5d94fccedbf07733f212
byc <2786660867@qq.com>
2026-04-17 22:31:54 +0800

    ci: configure changelogithub release permissions

---
+++ changelogithub.config.js
@@ -0,0 +1,15 @@
+export default {
+  types: {
+    perf: { title: "🔥 Performance" },
+    refactor: { title: "♻️ Refactors" },
+    docs: { title: "📖 Documentation" },
+    build: { title: "📦 Build" },
+    types: { title: "🌊 Types" },
+    chore: { title: "🏡 Chore" },
+    examples: { title: "🏀 Examples" },
+    test: { title: "✅ Tests" },
+    style: { title: "🎨 Styles" },
+    ci: { title: "🤖 CI" },
+    revert: { title: "⏪ Revert" },
+  },
+};
